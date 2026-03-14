168803a8e801f7014b732a9d57b9075b55279949
Peter Gangmei <topeter44msg@gmail.com>
2026-03-15 00:09:43 +0530

    update

--- config/settings/stage.py
+++ config/settings/stage.py
@@ -12,7 +12,6 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cloud.mepeter.com", "localhost", "127.0.0.1"])
-CSRF_TRUSTED_ORIGINS = ["https://cloud.mepeter.com"]
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
