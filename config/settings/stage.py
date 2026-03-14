9a266cfe12fb866a0e52fed1d03150b1c9c2bbc1
Peter Gangmei <topeter44msg@gmail.com>
2026-03-14 18:39:39 +0530

    feat: Upgrade Bootstrap to 5.3.3, introduce dark theme support, and expand allowed hosts for local development.

--- config/settings/stage.py
+++ config/settings/stage.py
@@ -11,7 +11,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cloud.mepeter.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cloud.mepeter.com", "localhost", "127.0.0.1"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
