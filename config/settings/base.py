e478565d2889a2a5091744344fefeec938e734ab
Peter Gangmei <topeter44msg@gmail.com>
2026-03-14 18:49:56 +0530

    add cloud app

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,6 +93,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "core.users",
+    "apps.cloud",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
